f0c6f748b4558ca52d9c41da22ac9b681dca0da1
KekePhasha <61992331+KekePhasha@users.noreply.github.com>
2025-05-20 20:21:05 +0200

    save files

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
-
-# Press ⌃F5 to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press F9 to toggle the breakpoint.
-
-
 # Press the green button in the gutter to run the script.
+from pose_estimation.vitpose_extractor import ViTPoseEstimator
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    estimator = ViTPoseEstimator
+    keypoints = estimator.extract_keypoints( 'dataset/videos/sample_video.mp4', save_name='sample_video_keypoints')
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
